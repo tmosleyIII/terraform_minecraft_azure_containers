40a52f720d9eaf6e50afbe642d51dbb1f19fe378
tmosleyiii <tmosleyiii@penguin>
2020-05-24 17:03:26 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,104 @@
+provider "azurerm" {
+    version = "=2.5.0"
+    features {}
+}
+
+terraform {
+    backend "remote" {
+        hostname = "app.terraform.io"
+        organization = "BizalignedSolutions"
+
+        workspaces {
+            name = "terraform_minecraft_azure_containers"
+        }
+    }
+}
+
+resource "random_password" "password" {
+    length  = 16
+    special = true
+    override_special = "_%@"
+}
+
+resource "azurerm_resource_group" "minecraft" {
+    name        = "hashicrafttest"
+    location    = "South Central US"
+}
+
+resource "azurerm_storage_account" "minecraft" {
+    name                     = "tmhashicrafttf"
+    resource_group_name      = azurerm_resource_group.minecraft.name
+    location                 = azurerm_resource_group.minecraft.location
+    account_tier             = "Standard"
+    account_replication_type = "GRS"
+
+    tags = {
+        environment = "production"
+    }
+}
+
+resource "azurerm_storage_share" "minecraft_world" {
+    name                 = "world"
+    storage_account_name = azurerm_storage_account.minecraft.name
+    quota                = 50
+}
+
+resource "azurerm_storage_share" "minecraft_config" {
+    name                 = "config"
+    storage_account_name = azurerm_storage_account.minecraft.name
+    quota                = 1
+}
+
+resource "azurerm_container_group" "minecraft" {
+    name                = "minecraft"
+    location            = azurerm_resource_group.minecraft.location
+    resource_group_name = azurerm_resource_group.minecraft.name
+    ip_address_type     = "public"
+    dns_name_label      = "tmhashicrafttf"
+    os_type             = "Linux"
+
+    container {
+        name    = "testserver"
+        image   = "hashicraft/minecraft:v1.12.2"
+        cpu     = "1"
+        memory  = "1"
+
+        # Main minecraft port
+        ports {
+            port        = 25565
+            protocol    = "TCP"
+        }
+
+        volume {
+            name                 = "world"
+            mount_path           = "/minecraft/world"
+            storage_account_name = azurerm_storage_account.minecraft.name
+            storage_account_key  = azurerm_storage_account.minecraft.primary_access_key
+            share_name           = azurerm_storage_share.minecraft_world.name
+        }
+
+        volume {
+            name                 = "config"
+            mount_path           = "/minecraft/config"
+            storage_account_name = azurerm_storage_account.minecraft.name
+            storage_account_key  = azurerm_storage_account.minecraft.primary_access_key
+            share_name           = azurerm_storage_share.minecraft_config.name
+        }
+
+        environment_variables = {
+            JAVA_MEMORY="1G",
+            MINECRAFT_MOTD="Hashicraft Test",
+            WHITELIST_ENABLED=true,
+            RCON_ENABLED=true,
+            RCON_PASSWORD=random_password.password.result
+        }
+    }
+}
+output "fqdn" {
+    value = azurerm_container_group.minecraft.fqdn
+}
+
+output "rcon_password" {
+    value = random_password.password.result
+}
+    
